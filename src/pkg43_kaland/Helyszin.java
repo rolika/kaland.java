b7ddc275e7517c58e40bd29c2289186e8cb07e2a
rolika <weisz.roland@invitel.hu>
2017-01-24 21:50:10 +0100

    helyszín osztály első megvalósítása

---
+++ src/pkg43_kaland/Helyszin.java
@@ -0,0 +1,52 @@
+package pkg43_kaland;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class Helyszin {
+  
+  private static final String SOTET = "Túl sötét van, nem látsz semmit!";
+  
+  private final String nev;
+  private final String leiras;
+  private boolean sotet;
+  private boolean bejart;
+  
+  private Helyszin(String nev, String leiras, boolean sotet) {
+    this.nev = nev;
+    this.leiras = leiras;
+    this.sotet = sotet;
+    bejart = false;
+  }
+  
+  Helyszin uj(ResultSet rs) throws SQLException {
+    return new Helyszin(rs.getString("nev"), rs.getString("leiras"), rs.getBoolean("sotet"));
+  }
+  
+  String getnev() {
+    return nev;
+  }
+  
+  boolean isSotet() {
+    return sotet;
+  }
+  
+  void setBejart(boolean bejart) {
+    this.bejart = bejart;
+  }
+  
+  void setSotet(boolean sotet) {
+    this.sotet = sotet;
+  }
+  
+  String helyszinLeiras() {
+    return sotet ? SOTET : (bejart ? nev : leiras);
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("%s: %ssötét, %sbejárt%n",
+      nev, sotet ? "" : "nincs ", bejart ? "" : "nem ");
+  }
+  
+}
