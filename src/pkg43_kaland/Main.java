84da3a42c6c2b9002a52270452de64e17ffb92ef
rolika <weisz.roland@pohlen-dach.hu>
2017-01-25 11:12:36 +0100

    jdbc teszt

--- src/pkg43_kaland/Main.java
+++ src/pkg43_kaland/Main.java
@@ -1,12 +1,21 @@
 package pkg43_kaland;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
     
-    Jatekter.main();
+    //Jatekter.main();
+    try (Connection kon = DriverManager.getConnection("jdbc:sqlite:kaland.sql")) {
+      SqliteJDBC sql = new SqliteJDBC(kon);
+      sql.minden().forEach(System.out::println);
+    } catch (SQLException ex) {
+      
+    }
     
   }
 
