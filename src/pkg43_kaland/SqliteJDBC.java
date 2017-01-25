87513b23eeeab3cb5a2d0f049b1c6cc217e0968f
rolika <weisz.roland@pohlen-dach.hu>
2017-01-25 11:13:52 +0100

    jdbc működésre bírva

--- src/pkg43_kaland/SqliteJDBC.java
+++ src/pkg43_kaland/SqliteJDBC.java
@@ -1,5 +1,7 @@
 package pkg43_kaland;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -7,6 +9,16 @@ import java.util.List;
 
 class SqliteJDBC {
   
+  private final PreparedStatement minden;
+  
+  public SqliteJDBC(Connection kon) throws SQLException {
+    minden = kon.prepareStatement("SELECT * FROM helyiseg");
+  }
+  
+  List<Helyszin> minden() throws SQLException {
+    return tobbTalalat(minden.executeQuery());
+  }
+    
   private Helyszin egyTalalat(ResultSet rs) throws SQLException {    
     return Helyszin.uj(rs);
   }
